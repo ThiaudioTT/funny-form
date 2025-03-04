725b1a24944c498c655e5738cff54ee7a896c762
ThiaudioTT <thiago.sky25@gmail.com>
2025-03-04 13:52:05 -0300

    feat: adds the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,90 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSnackBarModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'validation-closure-angular';
+  funnyForm: FormGroup;
+  isSubmitted = false;
+  formShake = false;
+  randomFeedback = '';
+
+  funnyFeedbacks = [
+    'This form is judging you silently.',
+    'Your form-filling skills need work.',
+    'Even my grandma could fill this form better.',
+    'Are you sure you want to submit this?',
+    "I've seen better form entries from cats walking on keyboards.",
+    'Is this really the best you can do?',
+    "Take your time, it's not like I have other forms to validate.",
+    'Your form is as incomplete as my will to live (this is just a joke, i complety fine).',
+  ];
+
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
+    this.funnyForm = this.fb.group({
+      superheroName: ['', [Validators.required, Validators.minLength(3)]],
+      secretTalent: ['', Validators.required],
+      reason: ['', [Validators.required, Validators.maxLength(100)]],
+    });
+  }
+
+  ngOnInit() {
+    this.funnyForm.valueChanges.subscribe(() => {
+      if (this.funnyForm.invalid && this.funnyForm.touched) {
+        this.randomFeedback =
+          this.funnyFeedbacks[
+            Math.floor(Math.random() * this.funnyFeedbacks.length)
+          ];
+      }
+    });
+  }
+
+  onSubmit() {
+    if (this.funnyForm.valid) {
+      this.isSubmitted = true;
+      this.snackBar.open('Form submitted successfully! (Not really)', 'Cool!', {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+      });
+    } else {
+      this.formShake = true;
+      setTimeout(() => {
+        this.formShake = false;
+      }, 500);
+
+      this.funnyForm.markAllAsTouched();
+      this.randomFeedback =
+        this.funnyFeedbacks[
+          Math.floor(Math.random() * this.funnyFeedbacks.length)
+        ];
+    }
+  }
 }
